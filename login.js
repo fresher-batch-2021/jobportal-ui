25ee94d174b376489f413c65b97f50817bf6c215
Shreeniveditha R <niveditharavichandran98@gmail.com>
2021-08-10 17:31:02 +0530

    Added files

--- login.js
+++ login.js
@@ -2,21 +2,33 @@ function login() {
     event.preventDefault();
     const email = document.querySelector("#email").value;
     const password = document.querySelector("#password").value;
-   
-    console.log(email + " " + password );
-    let userobj = {
-        "email": email,
-        "password": password,
-    }
-    console.log(userobj);
-    if(email =="nivi@gmail.com" && password =="123456")
-    {
-    alert("login Successfully");
-    window.location.href = "index.html";
+if(email ==""||email ==null||email.trim()==""){
+    alert("invalid username");
+} else{
+    if(password.trim()!=""){
+        const loginobj={
+            "email":email,
+           "password":password
+};
+const url="https://product-mock-api.herokuapp.com/jobportalapp/api/v1/auth/login"
+console.log(loginobj);
+axios.post(url,loginobj).then(res=>{
+    console.log(res);
+    alert("login succesful");
+    window.location.href="index.html";
+}).catch(err=>{
+    console.log(err.response.data);
+    if(err.response.data.errormessage){
+        alert(err.response.data.errormessage);
     }
     else{
-        alert("invalid user");
+        alert("login failed");
     }
-    
-
+});    
 }
+else{
+    alert("password is invalid");
+}
+}
+}
+    
--- register.js
+++ register.js
@@ -4,27 +4,31 @@ function register() {
     const password = document.querySelector("#password").value;
     const confirmpassword = document.querySelector("#confirmpassword").value;
    
-   
-    console.log(email + " " + password + "" + confirmpassword);
-    let userobj = {
-        "email": email,
-        "password": password,
-        "confirmpassword": confirmpassword,
-    }
-    console.log(userobj);
-    if(password == ""|| password == null ||password.trim()== "" ||password != confirmpassword){
-        alert("invalid password");
-    }
-    else{
-    if(password.length>=4)
-    {
-    alert("Registeration Successfully");
-    window.location.href = "login.html";
-    }
-    else{
-        alert("invalid password");
-    }
-    }
-    
-
+   if(email==""||email==null||email.trim()==""){
+       alert("invalid name");
+   }
+   if(password.length<6){
+       alert("password is short");
+   }
+   else 
+   if(password != confirmpassword){
+       alert("password does not match");
+   }
+   else{
+       let rejisterObj={
+           "email":email,
+           "password":password,
+           "confirmpassword":confirmpassword
+       };
+       console.log(rejisterObj);
+       const url="https://product-mock-api.herokuapp.com/jobportalapp/api/v1/auth/register"
+       axios.post(url,rejisterObj).then(res=>{
+           console.log(res);
+           alert("Registeration is succesful");
+           window.location.href="login.html";
+       }).catch(err=>{
+           console.error(err);
+           alert("Unable to register");
+       });
+   }
 }
